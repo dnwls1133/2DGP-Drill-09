1aee796b416d363758dfa57c230d94832a491ac1
dnwls1133 <117172889+dnwls1133@users.noreply.github.com>
2025-10-28 15:25:21 +0900

    수업 내용 까지 커밋 초기화

---
+++ game_world.py
@@ -0,0 +1,31 @@
+# layer 0: Background Objects
+# layer 1: Foreground Objects
+
+
+world = [[],[]]
+
+# 월드에 객체 추가
+def add_object(o,depth=0):
+    world[depth].append(o)
+
+def add_objects(ol,depth=0):
+    world[depth] += ol
+
+# 월드 전체의 객체들을 업데이트
+def update():
+    for layer in world:
+        for o in layer:
+            o.update()
+
+# 월드 전체의 객체들을 렌더링
+def render():
+    for layer in world:
+        for o in layer:
+            o.draw()
+def remove_object(o):
+    for layer in world:
+        if o in layer:
+            layer.remove(o)
+            return
+    else:
+        print("객체가 월드에 없습니다.")
